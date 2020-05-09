72be2844c9c2a9b03634aafe6c536d18252147e3
Pritam Sen <pritamsen@gmail.com>
2020-05-09 18:30:17 +0200

    Blog draft version
    Blog draft version; with dummy registration, login, create post feature

--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -1,6 +1,7 @@
 package technicalblog.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 import technicalblog.controller.HomeController;
 import technicalblog.model.Post;
 
@@ -36,5 +37,21 @@ public class PostService {
     posts.add(post2);
     posts.add(post3);
     return posts;
+  }
+
+  public ArrayList<Post> getOnePost() {
+    ArrayList<Post> posts = new ArrayList<>();
+
+    Post post1 = new Post();
+    post1.setTitle("This is your Post");
+    post1.setBody("This is your Post. It has some valid content");
+    post1.setDate(new Date());
+    posts.add(post1);
+
+    return posts;
+
+  }
+
+  public void createPost (Post newPost){
   }
 }
