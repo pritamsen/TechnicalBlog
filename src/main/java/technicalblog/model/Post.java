4e462fd7ebb353d64a0242d7d531229c4263b89d
Pritam Sen <pritamsen@gmail.com>
2020-05-09 16:02:12 +0200

    Post ervice created

--- src/main/java/technicalblog/model/Post.java
+++ src/main/java/technicalblog/model/Post.java
@@ -4,32 +4,31 @@ import java.util.Date;
 
 public class Post {
 
-    private String title;
-    private String body;
-    private Date date;
+  private String title;
+  private String body;
+  private Date date;
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public String getBody() {
-        return body;
-    }
+  public String getBody() {
+    return body;
+  }
 
-    public void setBody(String body) {
-        this.body = body;
-    }
+  public void setBody(String body) {
+    this.body = body;
+  }
 
-    public Date getDate() {
-        return date;
-    }
+  public Date getDate() {
+    return date;
+  }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
+  public void setDate(Date date) {
+    this.date = date;
+  }
 }
